c46f45b78504a9ed3f6bc5ae8ec81995ee96e030
josushell <yoy07030@khu.ac.kr>
2022-05-12 22:29:48 +0900

    0512

---
+++ 2022_05_12/Permutations_II.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <vector>
+
+// STL 사용한 버전
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        
+        sort(nums.begin(), nums.end());
+        
+        do
+        {
+            result.push_back(nums);
+        }while(next_permutation(nums.begin(), nums.end()));
+        
+        return result;
+    }
+};
+
+
+
