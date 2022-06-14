de6e72c16d038291fbd418ec2aa839b26d1cd15a
josushell <yoy07030@khu.ac.kr>
2022-06-14 11:29:11 +0900

    0614

---
+++ 2022_06_14_star/Delete_Operation_for_Two_Strings.cpp
@@ -0,0 +1,22 @@
+// DP + LCS
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        // 중복되는 가장 긴 문자열 찾기
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+        
+        for(int i = 1;i <= word1.size();i++)
+        {
+            for(int j = 1;j <= word2.size();j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return word1.size() + word2.size() - dp[word1.size()][word2.size()] * 2;
+
+    }
+};
